d65205f8834c5fc4e5cffa8dcdd7484618d94d05
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:27:59 +0530

    Update logs.py

--- handlers/logs.py
+++ handlers/logs.py
@@ -1,1 +1,24 @@
 #AxiomForceSub --by OwnerAxiom
+import os
+
+from pyrogram import Client, filters
+
+from config import OWNER_ID
+
+
+@Client.on_message(filters.private & filters.command("logs"))
+async def send_logs(client, message):
+
+    if message.from_user.id != OWNER_ID:
+        return
+
+    if not os.path.exists("logs/bot.log"):
+
+        return await message.reply_text(
+            "No Logs Found."
+        )
+
+    await message.reply_document(
+        "logs/bot.log",
+        caption="📄 Latest Bot Logs"
+    )
