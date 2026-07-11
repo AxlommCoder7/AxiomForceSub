4d00e4bca7a3e863ee744d999715c3e4cce103e7
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:14:22 +0530

    Update groups.py

--- database/groups.py
+++ database/groups.py
@@ -1,1 +1,16 @@
 #AxiomForceSub --by OwnerAxiom
+from .mongo import groups
+
+
+async def add_group(chat_id: int):
+    if not await groups.find_one({"_id": chat_id}):
+        await groups.insert_one({"_id": chat_id})
+
+
+async def get_groups():
+    async for chat in groups.find():
+        yield chat["_id"]
+
+
+async def total_groups():
+    return await groups.count_documents({})
