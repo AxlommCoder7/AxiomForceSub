fc04998178945668299e59df168182ac34cfdcc5
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
 #AxiomForceSub --by OwnerAxiom
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+LOGGER_ID = int(os.getenv("LOGGER_ID"))
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+FORCE_SUB = os.getenv("FORCE_SUB")
+
+GIT_REPO = os.getenv("GIT_REPO")
+GIT_BRANCH = os.getenv("GIT_BRANCH", "main")
