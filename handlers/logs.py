e5efeedf626d26876726025d04b193d23d73bb59
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:39:21 +0530

    Update logs.py

--- handlers/logs.py
+++ handlers/logs.py
@@ -7,7 +7,7 @@ from config import OWNER_ID
 
 
 @Client.on_message(filters.private & filters.command("logs"))
-async def send_logs(client, message):
+async def logs_handler(client, message):
 
     if message.from_user.id != OWNER_ID:
         return
@@ -15,10 +15,16 @@ async def send_logs(client, message):
     if not os.path.exists("logs/bot.log"):
 
         return await message.reply_text(
-            "No Logs Found."
+            "❌ Log file not found."
+        )
+
+    if os.path.getsize("logs/bot.log") == 0:
+
+        return await message.reply_text(
+            "⚠ Log file is empty."
         )
 
     await message.reply_document(
         "logs/bot.log",
-        caption="📄 Latest Bot Logs"
+        caption="📄 Latest Bot Log"
     )
