be91cf6ffb56fea039cd2cf3dcce074b3449ac5a
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 14:50:02 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,7 @@
 #AxiomForceSub --by OwnerAxiom
-from .mongo import *
-
 from .users import *
 from .groups import *
 from .channels import *
 from .settings import *
 from .stats import *
+from .bots import *
