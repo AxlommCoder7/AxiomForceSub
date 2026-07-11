fa8a217387051f238eb0f6fc17c965e03b1ef773
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:28:21 +0530

    Update gitpull.py

--- handlers/gitpull.py
+++ handlers/gitpull.py
@@ -1,1 +1,36 @@
 #AxiomForceSub --by OwnerAxiom
+import subprocess
+
+from pyrogram import Client, filters
+
+from config import OWNER_ID
+
+
+@Client.on_message(filters.private & filters.command("gitpull"))
+async def gitpull(client, message):
+
+    if message.from_user.id != OWNER_ID:
+        return
+
+    msg = await message.reply_text(
+        "🔄 Updating Repository..."
+    )
+
+    try:
+
+        result = subprocess.check_output(
+            "git pull",
+            shell=True,
+            stderr=subprocess.STDOUT
+        ).decode()
+
+    except Exception as e:
+
+        result = str(e)
+
+    if len(result) > 3900:
+        result = result[:3900]
+
+    await msg.edit_text(
+
+f"""✅ Git Pull Finished
