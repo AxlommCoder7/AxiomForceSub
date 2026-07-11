fb76ece0c1070bac364faaf1289f53758733404e
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:14:03 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,1 +1,20 @@
 #AxiomForceSub --by OwnerAxiom
+from .mongo import users
+
+
+async def add_user(user_id: int):
+    if not await users.find_one({"_id": user_id}):
+        await users.insert_one({"_id": user_id})
+
+
+async def is_user(user_id: int):
+    return await users.find_one({"_id": user_id})
+
+
+async def get_users():
+    async for user in users.find():
+        yield user["_id"]
+
+
+async def total_users():
+    return await users.count_documents({})
