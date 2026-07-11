95bd357464546ebd662ecd11925df842f79f3f15
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:09:58 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,16 @@
 #AxiomForceSub --by OwnerAxiom
+from pyrogram import Client
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import *
+
+app = Client(
+    "AxiomManagerBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+mongo = AsyncIOMotorClient(MONGO_URI)
+
+db = mongo["AxiomManagerBot"]
