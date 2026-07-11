10f8e97bd37ea00236b5e19971cee363982df31f
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:38:36 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,54 @@
 #AxiomForceSub --by OwnerAxiom
 import logging
+import traceback
+
+from loader import app
+from config import LOGGER_ID
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[
-        logging.FileHandler("logs/bot.log"),
+        logging.FileHandler("logs/bot.log", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
 
 LOGGER = logging.getLogger("AxiomManagerBot")
+
+
+async def send_log(text: str):
+
+    try:
+        await app.send_message(
+            LOGGER_ID,
+            text,
+            disable_web_page_preview=True
+        )
+    except Exception:
+        LOGGER.exception("Failed to send logger message.")
+
+
+async def send_error(error: Exception):
+
+    err = "".join(
+        traceback.format_exception(
+            type(error),
+            error,
+            error.__traceback__
+        )
+    )
+
+    if len(err) > 3900:
+        err = err[:3900]
+
+    try:
+
+        await app.send_message(
+            LOGGER_ID,
+            f"🚨 **Bot Error**\n\n```{err}```"
+        )
+
+    except Exception:
+
+        LOGGER.exception("Unable to send error log.")
