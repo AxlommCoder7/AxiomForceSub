97ebe242bf8a0dec0122f15b45155c349a94d0de
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:29:43 +0530

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,1 +1,19 @@
 #AxiomForceSub --by OwnerAxiom
+from functools import wraps
+
+from config import OWNER_ID
+
+
+def owner_only(func):
+
+    @wraps(func)
+
+    async def wrapper(client, message):
+
+        if message.from_user.id != OWNER_ID:
+
+            return
+
+        return await func(client, message)
+
+    return wrapper
