e57cce1538ffcf95d75d76a488f8fb0af2c3138c
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:29:26 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,20 @@
 #AxiomForceSub --by OwnerAxiom
+from pyrogram.enums import ChatType
+
+
+def is_private(message):
+
+    return message.chat.type == ChatType.PRIVATE
+
+
+def is_group(message):
+
+    return message.chat.type in (
+        ChatType.GROUP,
+        ChatType.SUPERGROUP
+    )
+
+
+def is_channel(message):
+
+    return message.chat.type == ChatType.CHANNEL
