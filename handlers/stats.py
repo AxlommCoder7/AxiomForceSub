363810f25ec40c05e05b42d82484c3f0c40e8372
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:26:09 +0530

    Update stats.py

--- handlers/stats.py
+++ handlers/stats.py
@@ -1,1 +1,27 @@
 #AxiomForceSub --by OwnerAxiom
+from pyrogram import Client
+from pyrogram import filters
+
+from config import OWNER_ID
+from database.stats import get_stats
+
+
+@Client.on_message(filters.private & filters.command("stats"))
+async def stats(client, message):
+
+    if message.from_user.id != OWNER_ID:
+        return
+
+    data = await get_stats()
+
+    await message.reply_text(
+        f"""
+📊 **Statistics**
+
+👤 Users : {data['users']}
+
+👥 Groups : {data['groups']}
+
+📢 Channels : {data['channels']}
+"""
+    )
