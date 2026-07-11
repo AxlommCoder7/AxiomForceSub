1ce39c479f000c8d554d89ae41c59ae89d878f42
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:28:45 +0530

    Update git.py

--- utils/git.py
+++ utils/git.py
@@ -1,1 +1,19 @@
 #AxiomForceSub --by OwnerAxiom
+import subprocess
+
+
+def git_pull():
+
+    try:
+
+        out = subprocess.check_output(
+            "git pull",
+            shell=True,
+            stderr=subprocess.STDOUT
+        ).decode()
+
+        return True, out
+
+    except Exception as e:
+
+        return False, str(e)
