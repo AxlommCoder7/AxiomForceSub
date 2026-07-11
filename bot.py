796f6d32d8ff66a75f691800af336b37656cbe25
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:16:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 #AxiomForceSub --by OwnerAxiom
+from loader import app
+
+import handlers.start
+import handlers.admin
+import handlers.force_sub
+import handlers.broadcast
+import handlers.stats
+import handlers.logs
+import handlers.gitpull
+import handlers.callbacks
+
+
+app.run()
