ff9d49498ad5bc65c6756e49c02cbf3988fb40d5
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 14:54:21 +0530

    Update owner.py

--- filters/owner.py
+++ filters/owner.py
@@ -3,8 +3,12 @@ from pyrogram import filters
 from config import OWNER_ID
 
 
-def owner_filter(_, __, message):
-    return message.from_user and message.from_user.id == OWNER_ID
+async def _owner(_, __, message):
+
+    if not message.from_user:
+        return False
+
+    return message.from_user.id == OWNER_ID
 
 
-owner_filter = filters.create(owner_filter)
+owner_filter = filters.create(_owner)
