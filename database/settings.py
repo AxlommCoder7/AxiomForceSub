d397e82b13c89a088ad328659d080c4fd6a3663a
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:15:01 +0530

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -1,1 +1,17 @@
 #AxiomForceSub --by OwnerAxiom
+from .mongo import settings
+
+
+async def set_force_sub(chat_id):
+    await settings.update_one(
+        {"_id": "forcesub"},
+        {"$set": {"chat_id": chat_id}},
+        upsert=True,
+    )
+
+
+async def get_force_sub():
+    data = await settings.find_one({"_id": "forcesub"})
+    if data:
+        return data["chat_id"]
+    return None
