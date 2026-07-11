4882948e941088beccd3995ecd8a101e08fb40c9
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:15:21 +0530

    Update stats.py

--- database/stats.py
+++ database/stats.py
@@ -1,1 +1,12 @@
 #AxiomForceSub --by OwnerAxiom
+from .users import total_users
+from .groups import total_groups
+from .channels import total_channels
+
+
+async def get_stats():
+    return {
+        "users": await total_users(),
+        "groups": await total_groups(),
+        "channels": await total_channels(),
+    }
