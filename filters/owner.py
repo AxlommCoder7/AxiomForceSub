2d1d54b4424afeefaa358317caa7708941904f04
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:22:51 +0530

    Update owner.py

--- filters/owner.py
+++ filters/owner.py
@@ -1,1 +1,10 @@
 #AxiomForceSub --by OwnerAxiom
+from pyrogram import filters
+from config import OWNER_ID
+
+
+def owner_filter(_, __, message):
+    return message.from_user and message.from_user.id == OWNER_ID
+
+
+owner_filter = filters.create(owner_filter)
