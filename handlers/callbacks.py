618e4ea935479bf2effba47eb6ac242b9d6342ff
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:25:24 +0530

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,1 +1,51 @@
 #AxiomForceSub --by OwnerAxiom
+from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import CallbackQuery
+
+from database.settings import get_force_sub
+
+
+@Client.on_callback_query(filters.regex("^checksub$"))
+async def check_sub(client: Client, query: CallbackQuery):
+
+    force = await get_force_sub()
+
+    if not force:
+        return await query.answer(
+            "Force Subscribe Disabled.",
+            show_alert=True
+        )
+
+    try:
+
+        member = await client.get_chat_member(
+            force,
+            query.from_user.id
+        )
+
+        if member.status in (
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER,
+        ):
+
+            await query.message.edit_text(
+                f"""
+✅ Thank you {query.from_user.mention}.
+
+Subscription Verified.
+
+Now send /start
+"""
+            )
+
+            return
+
+    except:
+        pass
+
+    await query.answer(
+        "❌ You haven't joined yet.",
+        show_alert=True
+    )
