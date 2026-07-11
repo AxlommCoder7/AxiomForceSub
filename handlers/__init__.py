4614d69782bb48efde02ca66d079a892c3636a7b
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 16:45:49 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,21 +1,3 @@
 """
 Axiom ForceSub Handlers
 """
-
-from .start import *
-from .callbacks import *
-from .force_sub import *
-from .admin import *
-from .broadcast import *
-from .stats import *
-from .logs import *
-
-try:
-    from .gitpull import *
-except Exception:
-    pass
-
-try:
-    from .clone import *
-except Exception:
-    pass
