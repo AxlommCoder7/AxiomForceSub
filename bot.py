4cf46f42bfe2cf89f7ecc1dd6c73ea6513d2ba06
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:39:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,48 @@
 #AxiomForceSub --by OwnerAxiom
 from loader import app
 
+from utils.logger import LOGGER, send_log
+
 import handlers.start
-import handlers.admin
+import handlers.callbacks
 import handlers.force_sub
 import handlers.broadcast
-import handlers.stats
+import handlers.admin
 import handlers.logs
+import handlers.stats
 import handlers.gitpull
-import handlers.callbacks
 
 
-app.run()
+async def startup():
+
+    LOGGER.info("Bot Started")
+
+    await send_log(
+        "🟢 **Bot Started Successfully**"
+    )
+
+
+async def shutdown():
+
+    LOGGER.info("Bot Stopped")
+
+    await send_log(
+        "🔴 **Bot Stopped**"
+    )
+
+
+app.start()
+
+app.loop.run_until_complete(
+    startup()
+)
+
+LOGGER.info("Bot Running...")
+
+app.idle()
+
+app.loop.run_until_complete(
+    shutdown()
+)
+
+app.stop()
