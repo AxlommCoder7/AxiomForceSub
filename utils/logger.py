fd7de20c8678cb6267a308ae2a9457a1c2e97e81
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:16:05 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,13 @@
 #AxiomForceSub --by OwnerAxiom
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler("logs/bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger("AxiomManagerBot")
