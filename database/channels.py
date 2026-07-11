1643c3902bc955763fe50579f986189244818fba
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 13:14:45 +0530

    Update channels.py

--- database/channels.py
+++ database/channels.py
@@ -1,1 +1,16 @@
 #AxiomForceSub --by OwnerAxiom
+from .mongo import channels
+
+
+async def add_channel(chat_id: int):
+    if not await channels.find_one({"_id": chat_id}):
+        await channels.insert_one({"_id": chat_id})
+
+
+async def get_channels():
+    async for chat in channels.find():
+        yield chat["_id"]
+
+
+async def total_channels():
+    return await channels.count_documents({})
