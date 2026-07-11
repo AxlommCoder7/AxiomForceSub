3d734a0e243bd22b4f83e59690b21f7064eedb34
Axiom Coder <prettylioness0550@gmail.com>
2026-07-11 14:50:38 +0530

    Update groups.py

--- database/groups.py
+++ database/groups.py
@@ -1,16 +1,34 @@
 #AxiomForceSub --by OwnerAxiom
-from .mongo import groups
+from .mongo import GROUPS
 
 
 async def add_group(chat_id: int):
-    if not await groups.find_one({"_id": chat_id}):
-        await groups.insert_one({"_id": chat_id})
+
+    await GROUPS.update_one(
+        {"_id": chat_id},
+        {
+            "$setOnInsert": {
+                "_id": chat_id
+            }
+        },
+        upsert=True,
+    )
+
+
+async def delete_group(chat_id: int):
+
+    await GROUPS.delete_one(
+        {"_id": chat_id}
+    )
 
 
 async def get_groups():
-    async for chat in groups.find():
+
+    async for chat in GROUPS.find():
+
         yield chat["_id"]
 
 
 async def total_groups():
-    return await groups.count_documents({})
+
+    return await GROUPS.count_documents({})
